4390d3c99a5c70e0f8e87ccc075e6a3fbbeddd47
muskansinghal4 <113668386+muskansinghal4@users.noreply.github.com>
2024-06-07 19:14:40 +0530

    Add files via upload

--- Flask/apiflask.py
+++ Flask/apiflask.py
@@ -1,1 +1,15 @@
+# To create api using flask
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'This is an API'
+
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn) 
